59c57247db6677b38dba9f94354a6559c5e0d7e9
artir97 <85441675+artir97@users.noreply.github.com>
2024-03-16 22:46:00 +0100

    ADD: creating a task in Join with testcafe

--- tests/testing_all.js
+++ tests/testing_all.js
@@ -22,6 +22,7 @@ fixture`Hover through summary elements`
 
 test('Hover summary elements', async t => {
     await t
+        .maximizeWindow()
         .wait(2000)
         .hover(`.box-container > .summary-top > .summary-big-box`)
         .wait(1500)
@@ -35,8 +36,53 @@ test('Hover summary elements', async t => {
         .wait(1500)
         .hover('.box-container > .summary-bottom >.summary-small-box:nth-child(4)')
         .wait(1500)
+        .click('#taskPage')
 
 })
+
+
+fixture.only`Adding a Task`
+    .page`https://gruppe-782.developerakademie.net/add_task.html`
+
+test('Entering Title etc. to task', async t => {
+    const title = Selector('#add-task-title');
+    const description = Selector('#add-task-description');
+    const subTaskInput = Selector('#add-task-subtask-input');
+    const addSubTask = Selector('#add-task-subtask-image-container');
+    
+
+    await t 
+        .wait(1000)
+        .typeText(title, 'Trying out TestCafe')
+        .wait(1000)
+        .typeText(description, 'Creating a whole task only using TestCafe')
+        .wait(1000)
+        .typeText('#add-task-date', '2024-03-22')
+        .wait(1000)
+        .click('#add-task-urgent')
+        .wait(1000)
+        .click('#add-task-medium')
+        .wait(1000)
+        .click('#add-task-low')
+        .wait(1000)
+        .click('#add-task-assigne')
+        .wait(1000)
+        .click(Selector('.name').withText('TestCafe'))
+        .wait(1000)
+        .click('#searchbar-add-contacts-container > .rotated-image')
+        .wait(1000)
+        .click('#add-task-category')
+        .wait(1000)
+        .click(Selector('#add-task-category-dropdown > div').withText('Technical Task'))
+        .typeText(subTaskInput, 'This is a subtask created with TestCafe')
+        .click(addSubTask)
+        .wait(1000)
+        .click('#add-task-create-task')
+        
+
+})
+
+
 
 
 
