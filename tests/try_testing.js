84319a8d34daf7601bed5f86e50dadd51c59ff65
artir97 <85441675+artir97@users.noreply.github.com>
2024-03-21 22:38:50 +0100

    ADD: adding a few tests

--- tests/try_testing.js
+++ tests/try_testing.js
@@ -74,7 +74,7 @@ test('testing choosing gender', async t => {
     const radioFemale = Selector('#female');
     const radioOther = Selector('#other');
     const radioDoesNotExist = Selector('#doesNotExist');
-    
+
     await t
         .expect(radioMale.exists).ok()
         .expect(radioMale.value).eql('male')
@@ -88,4 +88,90 @@ test('testing choosing gender', async t => {
         .expect(radioDoesNotExist.exists).notOk()
 
         .click(radioMale);
+})
+
+test('testing dropdown', async t => {
+    const dropDown = Selector('#option');
+    const option1 = Selector('#option > option').withText('Option 1');
+    const option2 = Selector('#option > option').withText('Option 2');
+    const option3 = Selector('#option > option').withText('Option 3');
+
+    await t
+        .expect(dropDown.exists).ok()
+        .click(dropDown)
+
+        .expect(option1.exists).ok()
+        .expect(option2.exists).ok()
+        .expect(option3.exists).ok()
+
+        .click(option2);
+})
+
+test('testing selecting', async t => {
+    const option1 = Selector('#owc > option').withText('Option 1');
+    const option2 = Selector('#owc > option').withText('Option 2');
+    const option3 = Selector('#owc > option').withText('Option 3');
+    const option4 = Selector('#owc > option').withText('Option 4');
+
+    await t
+        .expect(option1.exists).ok()
+        .expect(option2.exists).ok()
+        .expect(option3.exists).ok()
+        .expect(option4.exists).notOk()
+
+        .click(option3);
+})
+
+test('testing selecting multiple options', async t => {
+    const option1 = Selector('input[type=checkbox][value="Option 1"]');
+    const option2 = Selector('input[type=checkbox][value="Option 2"]');
+    const option3 = Selector('input[type=checkbox][value="Option 3"]');
+
+
+    await t
+        .expect(option1.exists).ok()
+        .expect(option2.exists).ok()
+        .expect(option3.exists).ok()
+
+        .expect(option1.value).eql('Option 1')
+        .click(option1)
+
+        .expect(option2.value).eql('Option 2')
+        .click(option2)
+
+        .expect(option3.value).eql('Option 3')
+        .click(option3)
+})
+
+test('testing typing', async t => {
+    const guessInput = Selector('input[list="datalists"]');
+
+    await t
+        .expect(guessInput.exists).ok()
+        .scrollBy(0, 700)
+        .typeText(guessInput, 'Chocolate');
+})
+
+test('testing color choosing', async t => {
+    const colorPicker = Selector('#favcolor');
+
+    await t
+        .scrollBy(0, 700)
+        .expect(colorPicker.exists).ok()
+        .click(colorPicker)
+})
+
+test.only('testing range', async t => {
+    const numberInput = Selector('#quantity');
+
+    await t
+        .scrollBy(0, 1000)
+        .expect(numberInput.exists).ok()
+        .selectText(numberInput).pressKey('delete')
+        .typeText(numberInput, '123')
+
+    const numberInputValue = await numberInput.value;
+
+    await t
+        .expect(Number(numberInputValue)).gt(1)
 })
